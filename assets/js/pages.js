725c1ec25c37259c187c3ffbb8f607a4a43be7a8
Zacky Raechan <muhamadraechanulwanzacky@Jake.local>
2025-03-20 10:32:49 +0800

    First Commit

---
+++ assets/js/pages.js
@@ -0,0 +1,17 @@
+// JS untuk aktifin modal profile 
+document.addEventListener("DOMContentLoaded", function () {
+    const profilePic = document.querySelector(".main-header__pic img");
+    const menu = document.querySelector(".main-header__pic ul");
+
+    profilePic.addEventListener("click", function (event) {
+        menu.classList.toggle("active"); // Toggle class 'active'
+        event.stopPropagation(); // Hindari close saat klik gambar
+    });
+
+    // Klik di luar menu untuk menutup
+    document.addEventListener("click", function (event) {
+        if (!profilePic.contains(event.target) && !menu.contains(event.target)) {
+            menu.classList.remove("active");
+        }
+    });
+});
